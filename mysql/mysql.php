a1889f180e3f237341b062b1f9fc9cded4b01994
shihe@wanglibank.com <shihe@wanglibank.com>
2017-12-04 17:31:01 +0800

    w

---
+++ mysql/mysql.php
@@ -0,0 +1,9 @@
+$swoole_mysql = new Swoole\Coroutine\MySQL();
+$swoole_mysql->connect([
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'user' => 'root',
+    'password' => 'MyNewPass4!',
+    'database' => 'sd',
+]);
+$res = $swoole_mysql->query('select * from user');
