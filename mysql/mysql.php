125b2e4f88ecce68c0432f62614da6eb2e5dbd4b
shihe@wanglibank.com <shihe@wanglibank.com>
2017-12-05 13:42:56 +0800

    s

--- mysql/mysql.php
+++ mysql/mysql.php
@@ -8,25 +8,27 @@ $server = array(
     'password' => 'MyNewPass4!',
     'database' => 'sd',
     'charset' => 'utf8', //指定字符集
-    'timeout' => 2,  // 可选：连接超时时间（非查询超时时间），默认为SW_MYSQL_CONNECT_TIMEOUT（1.0）
+    'timeout' => 2,  // 可选：连接超时时间
 );
 $mysql->connect($server, function (Swoole\Mysql $db, $r) {
     if ($r === false) {
         var_dump($db->connect_errno, $db->connect_error);
         die;
     }
-});
     //start query
-$sql = 'select * from user';
+    $sql = 'select * from user';
+    
+    $db->query($sql, function (Swoole\Mysql $db, $r) {
+        if ($r === false) {
+            var_dump($db->error, $db->errno);
+        } elseif ($r === true) {
+            var_dump($db->affected_rows, $db->insert_id);
+        }
+        var_dump($r);
+    });
 
-$mysql->query($sql, function (Swoole\Mysql $db, $r) {
-    if ($r === false) {
-        var_dump($db->error, $db->errno);
-    } elseif ($r === true) {
-        var_dump($db->affected_rows, $db->insert_id);
-    }
-    var_dump($r);
 });
+
 
     
 $mysql->close();
