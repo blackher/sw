287a1053400d31b68ab01f387a5cb03c517e3fa0
shihe@wanglibank.com <shihe@wanglibank.com>
2017-12-05 10:43:15 +0800

    s

--- mysql/mysql.php
+++ mysql/mysql.php
@@ -1,12 +1,28 @@
 <?php
-    use Swoole\Coroutine;
-    $swoole_mysql = new Swoole\Coroutine\MySQL();
+    $swoole_mysql = new Swoole\MySQL();
     $swoole_mysql->connect([
         'host' => '127.0.0.1',
         'port' => 3306,
         'user' => 'root',
         'password' => 'MyNewPass4!',
         'database' => 'sd',
-    ]);
-    $res = $swoole_mysql->query('select * from user');
-    print_r($res);   
+    ],function(Swoole\Mysql $db,$r){
+        if($r == false){
+            var_dump($db->connect_errno, $db->connect_error);
+            die;
+        }
+    });
+
+    $sql = 'select * from user';
+
+
+
+    $swoole_mysql->query($sql, function (Swoole\Mysql $db, $r) {
+        if ($r === false) {
+            var_dump($db->error, $db->errno);
+        } elseif ($r === true) {
+            var_dump($db->affected_rows, $db->insert_id);
+        }
+        var_dump($r);
+    });
+     
