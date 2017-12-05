c56bfbcf8f2475c6b8499ca91f0365a71a5b0d4c
shihe@wanglibank.com <shihe@wanglibank.com>
2017-12-05 10:52:36 +0800

    a

--- mysql/mysql.php
+++ mysql/mysql.php
@@ -1,28 +1,29 @@
 <?php
-    $swoole_mysql = new Swoole\MySQL();
-    $swoole_mysql->connect([
-        'host' => '127.0.0.1',
-        'port' => 3306,
-        'user' => 'root',
-        'password' => 'MyNewPass4!',
-        'database' => 'sd',
-    ],function(Swoole\Mysql $db,$r){
-        if($r == false){
-            var_dump($db->connect_errno, $db->connect_error);
-            die;
-        }
-    });
+global $mysql;
+$mysql = new Mysql();
+$server = array(
+    'host' => '192.168.11.98',
+    'port' => 3306,
+    'user' => 'test',
+    'password' => 'test',
+    'database' => 'test',
+    'charset' => 'utf8', //指定字符集
+    'timeout' => 2,  // 可选：连接超时时间（非查询超时时间），默认为SW_MYSQL_CONNECT_TIMEOUT（1.0）
+);
 
-    $sql = 'select * from user';
-
-
-
-    $swoole_mysql->query($sql, function (Swoole\Mysql $db, $r) {
-        if ($r === false) {
-            var_dump($db->error, $db->errno);
-        } elseif ($r === true) {
-            var_dump($db->affected_rows, $db->insert_id);
-        }
-        var_dump($r);
-    });
+$mysql->connect($server, function (Mysql $db, $r) {
+    if ($r === false) {
+        var_dump($db->connect_errno, $db->connect_error);
+        die;
+    }
+});
+$sql = 'show tables';
+$mysql->query($sql, function (Mysql $db, $r) {
+    if ($r === false) {
+        var_dump($db->error, $db->errno);
+    } elseif ($r === true) {
+        var_dump($db->affected_rows, $db->insert_id);
+    }
+    var_dump($r);
+});
      
