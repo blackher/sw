5f66019a4fcc3d0cc8700b521ad800319299cb4f
root <root@localhost.localdomain>
2017-12-05 11:08:24 +0800

    b

--- mysql/mysql.php
+++ mysql/mysql.php
@@ -1,6 +1,6 @@
 <?php
 global $mysql;
-$mysql = new Mysql();
+$mysql = new Swoole\Mysql();
 $server = array(
     'host' => '192.168.11.98',
     'port' => 3306,
@@ -10,14 +10,14 @@ $server = array(
     'charset' => 'utf8', //指定字符集
     'timeout' => 2,  // 可选：连接超时时间（非查询超时时间），默认为SW_MYSQL_CONNECT_TIMEOUT（1.0）
 );
-$mysql->connect($server, function (Mysql $db, $r) {
+$mysql->connect($server, function (Swoole\Mysql $db, $r) {
     if ($r === false) {
         var_dump($db->connect_errno, $db->connect_error);
         die;
     }
 });
 $sql = 'show tables';
-$mysql->query($sql, function (Mysql $db, $r) {
+$mysql->query($sql, function (Swoole\Mysql $db, $r) {
     if ($r === false) {
         var_dump($db->error, $db->errno);
     } elseif ($r === true) {
