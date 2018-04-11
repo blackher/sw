5bec78b98f409ab35155f2a22ffc84b01564ce5f
shihe <shihe@wanglibank.com>
2018-04-11 11:22:59 +0800

    update controller

--- process/process2.php
+++ process/process2.php
@@ -9,23 +9,23 @@ class Process1
 {
 	public $mpid=0;//当前进程id
     public $works=[];//记录子进程
-    public $max_precess=1;//最多进程数
+    public $max_precess=10;//最多进程数
     public $new_index=0;//记录子进程数
-    
-    public $max_child =10;//最大子进程数
+
+    public $max_child =800;//最大子进程数  不能设置的过小 造成堵塞
     public function  __construct(){
 
     	 try {
-            declare(ticks=1);//每执行一次低级语句会检查一次该进程是否有未处理过的信号		
+            declare(ticks=1);//每执行一次低级语句会检查一次该进程是否有未处理过的信号
             ini_set("memory_limit","80M");//防止内存过小
     	    // install signal handler for dead kids
             pcntl_signal(SIGCHLD, [$this, "sig_handler"]);  //参考简书 https://www.jianshu.com/p/54ffd360454f
 
         	//这就导致一个问题：当执行N个任务之后，任务系统空闲的时候主进程是阻塞的，而在发生阻塞的时候子进程还在执行，所以就无法完成最后几个子进程的进程回收。。。
 
-			//process.php 就有这个问题  直接内存cpu 消耗太大
+			  //process.php 就有这个问题  直接内存cpu 消耗太大
 
-	   $this->redis = new Redis();
+	         $this->redis = new Redis();
            $this->redis->connect('192.168.11.98', 6379); //连接Redis
            swoole_set_process_name(sprintf('php-ps:%s', 'master'));//进程命名
            $this->mpid = posix_getpid();//获取当前进程id
@@ -38,9 +38,10 @@ class Process1
     }
 
     public function run(){
-        for ($i=0; $i < $this->max_precess; $i++) {
-            $this->CreateProcess();
-        }
+        ///for ($i=0; $i < $this->max_precess; $i++) {
+             $this->CreateProcess();
+        //}
+				//$this->CreateProcess();
     }
 
 
@@ -51,36 +52,38 @@ class Process1
      		# code...
      	    //限制子进程数量
             if($this->new_index <= $this->max_child){
-    	     	$data = $this->redis->rpop('process');//rpop  阻塞
-    	     	if(!$data){
-    			sleep(1);
-    	     		continue;//没有数据
-    	     	}
-    	        $process = new swoole_process(function(swoole_process $worker)use($index){
-    	            if(is_null($index)){
-    	                $index=$this->new_index;
-    	                $this->new_index++;
-    	            }//是否是新增子进程
-    	            swoole_set_process_name(sprintf('php-ps:%s',$index));//重新命名当前子进程
-                    $this->checkMpid($worker);
-    	            $recv = $worker->pop();            //recive data to master
-    		        
-                	sleep(rand(1, 3));//模拟耗时
-                	echo "From Master: {$recv}\n";
-              
-                	exit;
-                
+    	     		$data = $this->redis->rpop('process');//rpop  阻塞
+    	     		if(!$data){
+    						//sleep(1);
+    	     			continue;//没有数据
+    	     		}
+						$this->new_index++;
+						//echo "has {$this->new_index}child process";
+    	      $process = new swoole_process(function(swoole_process $worker)use($index){
+    	          if(is_null($index)){
+    	              $index=$this->new_index;
+    	              $this->new_index++;
+    	           }//是否是新增子进程
+    	          swoole_set_process_name(sprintf('php-ps:%s',$this->new_index));//重新命名当前子进程
+                //$this->checkMpid($worker);
+    	          $recv = $worker->pop();            //recive data to master
+                	//sleep(rand(1, 3));//模拟耗时
+                	//echo "From Master: {$recv}\n";
+									echo "this->new_index :{$this->new_index}\n";
+                	//exit;
+
 
 	        }, false, false);
             $process->useQueue();
-	    $pid=$process->start();  //执行fork系统调用，启动进程 放回子进程pid。
+	          $pid=$process->start();  //执行fork系统调用，启动进程 放回子进程pid。
             $process->push($data);
-	        $this->works[$index]=$pid;//记录当前pid
+	          $this->works[$index]=$pid;//记录当前pid
+						//$this->processWait();
         }
 	        //return $pid;
     	}
     }
-    //杀死主进程 
+    //杀死主进程
     public function checkMpid(&$worker){
         if(!swoole_process::kill($this->mpid,0)){
             $worker->exit();
@@ -90,30 +93,31 @@ class Process1
     }
 
     //重启子进程
-    // public function rebootProcess($ret){
-    //     $pid=$ret['pid'];
-    //     $index=array_search($pid, $this->works);//判断是否释放掉
-    //     if($index!==false){//未释放掉  重新启用
-    //         $index=intval($index);
-    //         $new_pid=$this->CreateProcess($index);
-    //         echo "rebootProcess: {$index}={$new_pid} Done\n";
-    //         return;
-    //     }
-    //     throw new \Exception('rebootProcess Error: no pid');
-    // }
+    public function rebootProcess($ret){
+        $pid=$ret['pid'];
+        $index=array_search($pid, $this->works);//判断是否释放掉
+        if($index!==false){//未释放掉  重新启用
+            $index=intval($index);
+            $new_pid=$this->CreateProcess($index);
+            echo "rebootProcess: {$index}={$new_pid} Done\n";
+            return;
+        }
+        throw new \Exception('rebootProcess Error: no pid');
+    }
     //回收   子进程结束必须要执行wait进行回收，否则子进程会变成僵尸进程
-    // public function processWait(){
-    //     while(1) {
-    //         if(count($this->works)){//若子进程未空,代表任务空闲 //结束循环
-    //             $ret = swoole_process::wait();//空闲的子进程
-    //             if ($ret) {
-    //                 $this->rebootProcess($ret);
-    //             }
-    //         }else{
-    //             break;
-    //         }
-    //     }
-    // }
+    public function processWait(){
+        while(1) {
+            if(count($this->works)){//若子进程未空,代表任务空闲 //结束循环
+                $ret = swoole_process::wait();//空闲的子进程
+								echo "res: {$ret['pid']}";
+                if ($ret) {
+                    $this->rebootProcess($ret);
+                }
+            }else{
+                break;
+            }
+        }
+    }
     private function sig_handler($signo) {
 //        echo "Recive: $signo \r\n";
         switch ($signo) {
